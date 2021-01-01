33b5518c9ce8634b480ca9797c49e20662667112
DazWilkin <daz.wilkin@gmail.com>
2021-01-01 09:44:18 -0800

    Unique language tag

--- build.sh
+++ build.sh
@@ -4,8 +4,13 @@ REGISTRY="ghcr.io"
 PREFIX="akri-webhook"
 REPO="${REGISTRY}/dazwilkin/${PREFIX}"
 TAG=$(git rev-parse HEAD)
-
 IMAGE=${REPO}:${TAG}
 
+# Create unique repo:tag
 docker build --tag=${IMAGE} --file=./Dockerfile .
 docker push ${IMAGE}
+
+# Create language identifying repo:lang
+LANG="golang"
+docker tag ${IMAGE} ${REPO}:${LANG}
+docker push ${REPO}:${LANG}
