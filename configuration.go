ea7cd5e5e0c8cec687f0876f092e885078eec2c1
DazWilkin <daz.wilkin@gmail.com>
2020-12-31 18:21:06 -0800

    Use Kubernetes' `metav1.ObjectMeta`

--- configuration.go
+++ configuration.go
@@ -2,22 +2,21 @@ package main
 
 import (
 	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 type Configuration struct {
-	APIVersion string   `json:"apiVersion"`
-	Kind       string   `json:"kind"`
-	Metadata   Metadata `json:"metadata"`
-	Spec       Spec     `json:"spec"`
-}
-type Metadata struct {
-	Annotations   map[string]interface{} `json:"annotations"`
-	ManagedFields []interface{}          `json:"managedFields"`
-	Name          string                 `json:"name"`
-	Namespace     string                 `json:"namespace"`
+	APIVersion string             `json:"apiVersion"`
+	Kind       string             `json:"kind"`
+	Metadata   *metav1.ObjectMeta `json:"metadata"`
+	Spec       Spec               `json:"spec"`
 }
 type Spec struct {
-	BrokerPodSpec *v1.PodSpec            `json:"brokerPodSpec"`
-	Capacity      int                    `json:"capacity"`
-	Protocol      map[string]interface{} `json:"protocol"`
+	BrokerPodSpec            *v1.PodSpec            `json:"brokerPodSpec,omitempty"`
+	Capacity                 int                    `json:"capacity"`
+	ConfigurationServiceSepc *v1.ServiceSpec        `json:"configurationServiceSpec,omitempty"`
+	InstanceServiceSpec      *v1.ServiceSpec        `json:"instanceServiceSpec,omitempty"`
+	Properties               map[string]string      `json:"properties,omitempty"`
+	Protocol                 map[string]interface{} `json:"protocol"`
+	Units                    string                 `json:"units"`
 }
